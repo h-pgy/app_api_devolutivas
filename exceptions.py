b4eed179ea0f8c063b2ed3b4343be39917fef234
h-pgy <hpougy@gmail.com>
2021-08-18 09:51:53 -0300

    Revert "project exceptions"
    This reverts commit f10763e52e861f96e60ff86f73ef8ad30de9ab23.

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-#project-specific exceptions
-
-class ResultadoVazio(ValueError):
-    '''Raised para mostrar quando acabaram os resultados de uma busca'''
-
-class MuitasRequisicoes(ValueError):
-    '''Raised para mostrar quando foram feitas requisicoes demais'''
