28dbcda06a5b4ca4bb551f4f71f32c762750689f
h-pgy <hpougy@gmail.com>
2021-08-18 09:53:27 -0300

    simple in-memory cache

--- cache.py
+++ cache.py
@@ -13,3 +13,16 @@ class Cache:
             dici_objeto = dici_tipo.get(id_obj,{})
             
             return dici_objeto.get(offset)
+
+    def add(self, result_busca, search_data, offset):
+        
+        for tipo_obj, id_obj in search_data.items():
+            if tipo_obj not in self.cache_dict:
+                self.cache_dict[tipo_obj] = {}
+            dici_tipo = self.cache_dict[tipo_obj]
+            if id_obj not in dici_tipo:
+                dici_tipo[id_obj] = {}    
+            dici_objeto = dici_tipo[id_obj]
+            
+            if offset not in dici_objeto:
+                dici_objeto[offset] = result_busca
